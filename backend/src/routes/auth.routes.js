829f7ebbe3517bd2ff31eb53e8aca86037075336
nayakkrati1-ai <nayakkrati1@gmail.com>
2026-02-20 16:37:00 +0530

    Fix : Added swagger for ApIDocumentation

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,51 @@
 import { registerUser, loginUser } from "../controllers/auth.controller.js";
 
 export async function authRoutes(fastify){
-    fastify.post('/register', registerUser);
-    fastify.post('/login', loginUser);
+    fastify.post('/register', {
+        schema: {
+          description: 'Register a new user',
+          tags: ['Auth'],
+          body: {
+            type: 'object',
+            required: ['name', 'email', 'password'],
+            properties: {
+              name: { type: 'string' },
+              email: { type: 'string' },
+              password: { type: 'string' }
+            }
+          },
+          response: {
+            201: {
+              type: 'object',
+              properties: {
+                message: { type: 'string' },
+                token: { type: 'string' }
+              }
+            }
+          }
+        }
+      }, registerUser);
+      
+    fastify.post('/login', {
+        schema: {
+          description: 'Login user',
+          tags: ['Auth'],
+          body: {
+            type: 'object',
+            required: ['email', 'password'],
+            properties: {
+              email: { type: 'string' },
+              password: { type: 'string' }
+            }
+          },
+          response: {
+            200: {
+              type: 'object',
+              properties: {
+                token: { type: 'string' }
+              }
+            }
+          }
+        }
+      }, loginUser);
 }
