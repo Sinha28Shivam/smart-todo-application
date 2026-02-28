3ad4a0dd29e5ab921df22449c4d5554e3f5dd88f
nayakkrati1-ai <nayakkrati1@gmail.com>
2026-02-28 22:36:17 +0530

    Implemented groqsdk for the cloud ai response

--- backend/src/routes/ai.route.js
+++ backend/src/routes/ai.route.js
@@ -1,6 +1,26 @@
 import { getAIResponse } from "../controllers/ai.controller.js";
 
+// export async function aiRoutes(fastify){
+//     fastify.addHook('preHandler', fastify.authenticate);
+//     fastify.post('/ai/suggest', getAIResponse);
+// }
+
 export async function aiRoutes(fastify){
-    fastify.addHook('preHandler', fastify.authenticate);
-    fastify.post('/ai/suggest', getAIResponse);
+ 
+fastify.post('/ai/suggest', {
+    preHandler: [fastify.authenticate],
+    schema: {
+      tags: ['AI'],
+      security: [{ BearerAuth: [] }],
+      body: {
+        type: 'object',
+        required: ['description', 'dueDate'],
+        properties: {
+          description: { type: 'string' },
+          dueDate: { type: 'string' }
+        }
+      }
+    }
+  }, getAIResponse);
+ 
 }
